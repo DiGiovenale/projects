4078a76825f84b805779900140283bbc3b01ffb7
digiovenale <45010741+digiovenale@users.noreply.github.com>
2018-11-19 10:25:04 +0100

    Update access_form.py

--- mysql_interface/pkg/access_form.py
+++ mysql_interface/pkg/access_form.py
@@ -4,7 +4,7 @@ from .main import *
 class AccessForm():
 
    '''
-   Creates a form, which asks the user to insert user and password for mysql connection.
+   The class creates a form, which asks the client to insert user and password to MySQL db.
    Then checks if the insert data enables a connection via pymysql.
    If that is the case, closes the form and return a self.status=True
    '''
@@ -24,6 +24,7 @@ class AccessForm():
       self.ent_usr.grid(row=0,column=1)
       self.ent_usr.focus_set()
       self.ent_usr.bind('<Return>',lambda x: self.ent_psw.focus_set())
+      self.ent_usr.bind('<Down>',lambda x: self.ent_psw.focus_set())
 
       self.lab_psw=tk.Label(self.root,text='Password:')
       self.lab_psw.grid(row=1,column=0)
@@ -31,6 +32,7 @@ class AccessForm():
       self.ent_psw=tk.Entry(self.root)
       self.ent_psw.grid(row=1,column=1)
       self.ent_psw.bind('<Return>',self.check_input)
+      self.ent_psw.bind('<Up>',lambda x: self.ent_usr.focus_set())
 
       self.conf=tk.Button(self.root,text='Confirm',fg='red',command=self.check_input)
       self.conf.bind('<Return>',self.check_input)
@@ -41,7 +43,8 @@ class AccessForm():
    def check_input(self,event=''):
       '''
       When called, verifies if the data provided in ent_usr and ent_psw are valid for a connection.
-      '''      
+      '''
+      
       try:
          self.connection=pymysql.connect(user=self.ent_usr.get(),password=self.ent_psw.get())
       except:
